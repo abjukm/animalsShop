afd1ac7a0d1c433f6532a57fd09fd230e1f6aecc
kirito <tilekovdekjan999@gmail.com>
2025-04-26 00:08:38 +0600

    commit name

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,10 +1,65 @@
 import React from "react";
 import Header from "../components/Header";
+import AnimalsCard from "../helpers/AnimalsCard";
+import bea from "../assets/dogsimg/beagle.jpg"
+import coc from "../assets/dogsimg/cocker.jpg"
+import king from "../assets/dogsimg/king.jpg"
+import korgi from "../assets/dogsimg/korgi.jpg"
+import mal from "../assets/dogsimg/maltipu.jpg"
+import pug from '../assets/dogsimg/pug.jpg'
 
 const Dogs = () => {
   return (
     <>
       <Header />
+      <div className="mt-35 w-[80%] ml-[10%] flex justify-around">
+        <AnimalsCard
+          image={bea}
+          name="Макс"
+          gender="Мужской"
+          age="3 года"
+          breed="Бигль"
+        />
+        <AnimalsCard
+          image={coc}
+          name="Луна"
+          gender="Женский"
+          age="3 года"
+          breed="Кокер спаниель"
+        />
+      </div>
+      <div className="mt-35 w-[80%] ml-[10%] flex justify-around">
+        <AnimalsCard
+          image={king}
+          name="Белла"
+          gender="Женский"
+          age="2 года"
+          breed="Кинг чарльз"
+        />
+        <AnimalsCard
+          image={korgi}
+          name="Рекс"
+          gender="Мужской"
+          age="1 года"
+          breed="Корги"
+        />
+      </div>
+      <div className="mt-35 w-[80%] ml-[10%] flex justify-around">
+        <AnimalsCard
+          image={mal}
+          name="Барни"
+          gender="Мужской"
+          age="1 года"
+          breed="Мальтипу"
+        />
+        <AnimalsCard
+          image={pug}
+          name="Чарли"
+          gender="Мужской"
+          age="1 года"
+          breed="Мопс"
+        />
+      </div>
     </>
   );
 };
